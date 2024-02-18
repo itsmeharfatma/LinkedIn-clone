6b367880fe076b421c07f28a771bcf924ff864d6
Mehar <mehar.fatma202@gmail.com>
2024-02-18 23:36:16 +0530

    added logo and favicon

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='header font-bold'>
+    <div className='header'>
       {/* header-left */}
       <div className=''>
-        <img src='' alt='' />
+        <img
+          src='/assets/linkedin-logo-48.png' alt='LinkedIn Logo'
+          // width={65}
+        />
 
+        {/* search bar */}
         <div className='search'>
-          <input type='text' />
+          <i class="fa-solid fa-magnifying-glass" style={{ color: "#000000" }}></i>
+          <input type='text' className='border-2' />
         </div>
       </div>
 
