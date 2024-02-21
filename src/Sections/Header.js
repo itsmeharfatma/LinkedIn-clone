d28a8670e2c09d0639e17769ff90b96120854cd0
Mehar <mehar.fatma202@gmail.com>
2024-02-21 22:34:38 +0530

    added left section of header

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -2,18 +2,19 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className='header'>
+    <header className='sticky flex'>
       {/* header-left */}
-      <div className=''>
+      <div className='flex items-center'>
         <img
-          src='/assets/linkedin-logo-48.png' alt='LinkedIn Logo'
-          // width={65}
+          src='/assets/linkedin-logo-48.png'
+          alt='LinkedIn Logo'
+          className='object-contain'
         />
 
         {/* search bar */}
-        <div className='search'>
-          <i class="fa-solid fa-magnifying-glass" style={{ color: "#000000" }}></i>
-          <input type='text' className='border-2' />
+        <div className='flex items-center px-4 py-[18px] h-6 gap-2 bg-[#eef3f8] rounded'>
+          <i class="fa-solid fa-magnifying-glass" style={{ color: "#808080" }}></i>
+          <input type='text' placeholder='Search' className='outline-none border-none bg-[#eef3f8]' />
         </div>
       </div>
 
@@ -21,7 +22,7 @@ const Header = () => {
       <div className=''>
 
       </div>
-    </div>
+    </header>
   )
 }
 
